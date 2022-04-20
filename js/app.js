1aa486e0f35ed45d4564d689548d3d5280ea3649
Cheyenne Piper <72780256+PiperRc@users.noreply.github.com>
2022-04-19 23:51:02 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-// javascript
+
+// about.html
+
+const quote = document.querySelector('.quote');
+const quoteAuthor=document.querySelector('.quote-author')
+
+const getQuote = async () => {
+    try{
+    const rand = Math.floor(Math.random() * 1644)
+    const res = await fetch("https://type.fit/api/quotes");
+    const data = await res.json();
+    quote.innerHTML=data[rand].text;
+    quoteAuthor.innerHTML=`-${data[rand].author}`;
+    }
+    catch{
+        quote.innerHTML="Something went wrong";
+    }
+}
+
+
+window.addEventListener('load',()=>{
+    getQuote();
+})
+
+// 
